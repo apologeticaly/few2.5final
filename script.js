3a6ed8224abf7b43ef8fde4e6812bfc7230f82ab
constantino <44826802+apologeticaly@users.noreply.github.com>
2021-03-01 12:04:20 -0800

    finished

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 import data from './data/data.js'
 
-// TODO Challenge 1
+// Challenge 1
 document.getElementById("subtitle").innerHTML = "FEW 2.5: Muhammed Constantino";
 
-// TODO Challenge 2
+// Challenge 2
 document.getElementById("info-name").innerHTML = "Name: World Happiness Report";
 document.getElementById("info-year").innerHTML = "Year: 2019";
 document.getElementById("info-countries").innerHTML = `Number of Countries: ${data.length}`;
 
-// TODO Challenge 3 
+// Challenge 3 
 
 document.getElementById("happiestbtn").addEventListener("click", function(){sortData('score');});
 document.getElementById("gdpbtn").addEventListener("click", function(){sortData('gdp');});
@@ -28,6 +28,7 @@ function sortData(category) {
     })
 
     displayTable(newdata, category)
+    displayGraph(category)
 }
 
 function displayTable(data, mcategory) {
@@ -90,8 +91,62 @@ function displayTable(data, mcategory) {
     element.insertBefore(main, element.firstChild);
 
 }
-
-
-
   
 // TODO Challenge 4
+
+function displayGraph(mcategory) {
+    document.getElementById('bycategorygraph').innerHTML = "";
+
+    var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#bycategorygraph")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Parse the Data
+    d3.json("./data/data.json", function(data) {
+
+        // sort data
+        data.sort(function(b, a) {
+            return a[mcategory] - b[mcategory];
+        });
+
+        // X axis
+        var x = d3.scaleBand()
+            .range([ 0, width ])
+            .domain(data.slice(0,10).map(function(d) { return d.country; }))
+            .padding(0.2);
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "translate(-10,0)rotate(-45)")
+            .style("text-anchor", "end");
+
+        // Add Y axis
+        var y = d3.scaleLinear()
+            .domain([0, 10])
+            .range([ height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // Bars
+        svg.selectAll("mybar")
+            .data(data.slice(0,10))
+            .enter()
+            .append("rect")
+            .attr("x", function(d) { return x(d.country); })
+            .attr("y", function(d) { return y(d[mcategory]); })
+            .attr("width", x.bandwidth())
+            .attr("height", function(d) { return height - y(d[mcategory]); })
+            .attr("fill", "#69b3a2")
+
+        })
+}
